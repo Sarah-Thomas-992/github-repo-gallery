4126a56e81bc9059b907cfb73c2b1609b90f2848
Sarah Thomas <sbade92@gmail.com>
2023-05-31 08:15:59 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Select the div with class overview
+const overview = document.querySelector(".overview");
+
+const username = "sarah-thomas-992";
+
+const getData = async function () {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+    // console.log(data);
+    displayInfo(data);
+};
+
+getData();
+
+const displayInfo = function (data) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("user-info");
+    newDiv.innerHTML = ` 
+    <figure>
+        <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;
+    overview.append(newDiv);
+};
